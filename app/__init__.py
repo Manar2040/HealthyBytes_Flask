9f38cdb3082ac478ca5e9997db9edf5a44babdcc
Aya Ayman <aymanayaayman406@gmail.com>
2026-05-08 02:41:05 +0300

    Implement full internationalization (AR/FR) and fix UI bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, request, session
+# pyrefly: ignore [missing-import]
 from flask_sqlalchemy import SQLAlchemy
+# pyrefly: ignore [missing-import]
 from flask_login import LoginManager
+# pyrefly: ignore [missing-import]
 from flask_bcrypt import Bcrypt
+# pyrefly: ignore [missing-import]
+from flask_babel import Babel
 from app.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
+babel = Babel()
 login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
 
@@ -20,6 +26,42 @@ def create_app(config_class=Config):
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
+
+    def select_locale():
+        # Check if language is specified in query parameters
+        lang = request.args.get('lang')
+        if lang in app.config['LANGUAGES']:
+            session['lang'] = lang
+            return lang
+        # Check if language is stored in session
+        return session.get('lang') or request.accept_languages.best_match(app.config['LANGUAGES'])
+    
+    babel.init_app(app, locale_selector=select_locale)
+
+    # pyrefly: ignore [missing-import]
+    from flask_babel import get_locale, format_number, format_decimal, format_date, format_datetime
+    
+    def to_arabic_nums(number):
+        if get_locale().language != 'ar':
+            return number
+        
+        western_nums = "0123456789"
+        eastern_nums = "٠١٢٣٤٥٦٧٨٩"
+        num_str = str(number)
+        return num_str.translate(str.maketrans(western_nums, eastern_nums))
+
+    @app.context_processor
+    def inject_babel():
+        return dict(
+            get_locale=get_locale, 
+            format_number=format_number, 
+            format_decimal=format_decimal,
+            format_date=format_date,
+            format_datetime=format_datetime,
+            ar_nums=to_arabic_nums
+        )
+    
+    app.jinja_env.filters['ar_nums'] = to_arabic_nums
 
     from app.blueprints.auth import auth
     from app.blueprints.recipes import recipes
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-
+from waitress import serve
 
 app = create_app()
 
@@ -8,4 +8,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    print("Server is starting using Waitress on http://127.0.0.1:5001")
+    serve(app, host='0.0.0.0', port=5001)
